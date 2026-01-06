a40a994a55482add2e15f945b098188b19db9bb2
Ved Sharma <sharmaved0009@gmail.com>
2026-01-06 19:51:02 +0530

    Filtering Values with the help of LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,35 +1,61 @@
 ﻿
 
 // Fetch Distinct Numbers from List using LINQ
-var listNumbers = new List<int>() { 1, 3, 4, 5, 2, 2, 2, 0, 6, 7 };
-var DistNumber = listNumbers.Distinct().ToList();
+        //var listNumbers = new List<int>() { 1, 3, 4, 5, 2, 2, 2, 0, 6, 7 };
+        //var DistNumber = listNumbers.Distinct().ToList();
 
-Console.WriteLine("--------------");
+        //Console.WriteLine("--------------");
 
-foreach (var number in DistNumber)
-{
-    Console.WriteLine($"Number is : " + number);
-}
+        //foreach (var number in DistNumber)
+        //{
+        //    Console.WriteLine($"Number is : " + number);
+        //}
 
-Console.WriteLine("--------------");
+        //Console.WriteLine("--------------");
 
 
 
 // Fetch Distinct Words from List using LINQ
-var listWords = new List<string>() { "Ved", "Abhi", "Monu", "Lucky", "Ved", "Monu"};
-        var DistWords = listWords.Distinct().ToList();
+        //var listWords = new List<string>() { "Ved", "Abhi", "Monu", "Lucky", "Ved", "Monu"};
+        //var DistWords = listWords.Distinct().ToList();
 
-        Console.WriteLine("--------------");
-        Console.WriteLine( "Distinct Words are :" );
-        foreach (var word in DistWords)
-        {
-            Console.WriteLine(word);
-        }
+        //Console.WriteLine("--------------");
+        //Console.WriteLine( "Distinct Words are :" );
+        //foreach (var word in DistWords)
+        //{
+        //    Console.WriteLine(word);
+        //}
 
-        Console.WriteLine("--------------");
-        Console.ReadLine();
-
-
+        //Console.WriteLine("--------------");
+        //Console.ReadLine();
 
 
+
+//Filtering
+
+var Filteringlist = new List<int>() { 1, 3, 4, 5, 2, 2, 2, 0, 6, 7 };
+var even = Filteringlist
+            .Where(x => x % 2 == 0)
+            .Distinct()
+            .ToList();
+var Odd = Filteringlist
+            .Where(x => x % 2 != 0)
+            .Distinct ()
+            .ToList();
+
+
+Console.WriteLine("Even Numbers");
+foreach (var number in even)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("Odd Numbers");
+foreach (var number in Odd)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("--------------");
+Console.ReadLine();
 

7b16e540a28fc25b23c41a31ab427e961648f678
Ved Sharma <sharmaved0009@gmail.com>
2026-01-06 19:27:58 +0530

    Topic 1: fetch distinct number from list

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,21 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿  
+
+        // Fetch Distinct Numbers from List using LINQ
+        var listNumbers = new List<int>() { 1, 3, 4, 5, 2, 2, 2, 0, 6, 7 };
+        var DistNumber = listNumbers.Distinct().ToList();
+
+        Console.WriteLine("--------------");
+
+        foreach (var number in DistNumber)
+        {
+            Console.WriteLine($"Number is : " + number);
+        }
+
+        Console.WriteLine("--------------");
+        Console.ReadLine();
+
+ 
+
+
+
+ 
